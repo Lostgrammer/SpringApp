c9c6aa6c88a83bd8472ceee51a1c5a6750db2e78
Carlos Vega <carlos_cmvp@hotmail.com>
2024-07-02 11:26:56 -0600

    generate new parameter to obtenerDatos() method  with the new url

--- src/main/java/com/carlosvega/ScreenMatch/ScreenMatchApplication.java
+++ src/main/java/com/carlosvega/ScreenMatch/ScreenMatchApplication.java
@@ -1,6 +1,7 @@
 package com.carlosvega.ScreenMatch;
 
 import com.carlosvega.ScreenMatch.model.ConvierteDatos;
+import com.carlosvega.ScreenMatch.model.DatosEpisodio;
 import com.carlosvega.ScreenMatch.model.DatosSerie;
 import com.carlosvega.ScreenMatch.service.ConsumoAPI;
 import org.springframework.boot.CommandLineRunner;
@@ -17,13 +18,18 @@ public class ScreenMatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoAPI = new ConsumoAPI();
-		String jsonResponse =  consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=breaking+bad&apikey=214a8fd4");
-		System.out.println(jsonResponse); //check json api
-		//deserialize json
+//		String jsonResponse =  consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=breaking+bad&apikey=214a8fd4");
+//		System.out.println(jsonResponse); //check json api response
+		//initialize conversor
 		ConvierteDatos conversor= new ConvierteDatos();
-		DatosSerie datosSerie1 = conversor.obtenerDatos(jsonResponse, DatosSerie.class); //usando como tipo de datros la clase DatosSerie
-		var datos = conversor.obtenerDatos(jsonResponse, DatosSerie.class);
-		System.out.println(datosSerie1);
-		System.out.println(datos);
+		//deserialize
+//		DatosSerie datosSerie1 = conversor.obtenerDatos(jsonResponse, DatosSerie.class); //usando como tipo de datos la clase DatosSerie
+//		var datos = conversor.obtenerDatos(jsonResponse, DatosSerie.class);
+//		System.out.println(datosSerie1);
+//		System.out.println(datos);
+		//deserialize episode one
+		String jsonEpisodeResponse = consumoAPI.obtenerDatos("https://www.omdbapi.com/?t=breaking+bad&Season=1&episode=1&apikey=214a8fd4");
+		var datosEpisodio = conversor.obtenerDatos(jsonEpisodeResponse, DatosEpisodio.class);
+		System.out.println(datosEpisodio);
 	}
 }
