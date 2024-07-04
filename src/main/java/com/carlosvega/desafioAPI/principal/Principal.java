269952f5bf6ef2fd473d378e1a8e9ec3ecf47575
Carlos Vega <carlos_cmvp@hotmail.com>
2024-07-04 16:20:24 -0600

    test ConsumoApi class with variables and input user amiibo name

--- src/main/java/com/carlosvega/desafioAPI/principal/Principal.java
+++ src/main/java/com/carlosvega/desafioAPI/principal/Principal.java
@@ -1,7 +1,24 @@
 package com.carlosvega.desafioAPI.principal;
 
+import com.carlosvega.ScreenMatch.service.ConsumoAPI;
+import com.carlosvega.desafioAPI.model.TransformaJson;
+
+import java.util.Scanner;
+
 public class Principal {
+    private final String URL_BASE = "https://www.amiiboapi.com/api/amiibo/";
+    private final String URL_NOMBRE_AMIIBO = "?name=";
+    Scanner input = new Scanner(System.in);
+    String amiiboNombre;
+    ConsumoAPI consumoAPI = new ConsumoAPI();
+    String json;
+
     public void mostrarMenu (){
-        System.out.println("funciona");
+        //mostrar json de amiibo especifico
+        System.out.println("ingrese el nombre de un amiibo que quiere buscar:");
+        amiiboNombre = input.nextLine();
+        json = consumoAPI.obtenerDatos(URL_BASE + URL_NOMBRE_AMIIBO + amiiboNombre);
+        System.out.println(json);
+
     }
 }
